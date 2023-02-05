db0cf06c2d49dff24403d5fb2bf46e58506f2167
Batraz Zaritskiy <batik1315@gmail.com>
2023-02-05 22:51:11 +0300

    added buttons delete

--- frontend/src/scripts/classes/PageEdit.js
+++ frontend/src/scripts/classes/PageEdit.js
@@ -9,11 +9,14 @@ export class PageEdit extends TemplatePage {
         <input type="text" class="form-control form-control-lg" placeholder="Category name" required>
       </div>
       <div class="col-12" data-container>
-        <div class="row g-3 pb-3">
-          <div class="col-md-6">
+        <div class="expression-group row g-3 pb-3">
+          <div class="col-1 expression-group__delete">
+            <input class="form-check-input" type="checkbox" value="">
+          </div>
+          <div class="col">
             <input type="text" class="form-control form-control-lg" placeholder="Ru" required>
           </div>
-          <div class="col-md-6">
+          <div class="col">
             <input type="text" class="form-control form-control-lg" placeholder="Eng" required>
           </div>
         </div>
@@ -21,12 +24,20 @@ export class PageEdit extends TemplatePage {
       <div class="col-12 mt-0">
         <a href="#add" class="button button--plus button--sm" title="Add new part"></a>
       </div>
-      <div class="col-12">
-        <button type="submit" class="btn btn-primary btn-lg">Done</button>
-      </div>
+      <div class="col-12 d-flex justify-content-between">
+        <button type="button" class="btn btn-primary btn-lg disabled" disabled>Delete items</button>
+        <div>
+          <button type="button" class="btn btn-primary btn-lg">Delete category</button>
+          <button type="submit" class="btn btn-primary btn-lg ms-3">Save</button>
+          </div>
+        </div>
     </form>
     `;
 
     return tmp;
+  }
+
+  getForm() {
+
   }
 }
